50cd9ca9464e0ae2cddb57bf772a1acb1097d301
Barbara Bonilla <bbonillasnchz@gmail.com>
2021-08-11 13:02:47 -0500

    Updated QuickviewService

--- apps/ecomm3/src/app/services/quickview.service.ts
+++ apps/ecomm3/src/app/services/quickview.service.ts
@@ -1,31 +1,33 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { Observable, of, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { Product } from '../interfaces/product';
-import { map, takeUntil } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root',
 })
 export class QuickviewService implements OnDestroy {
-    private destroy$: Subject<void> = new Subject();
     private abortPrevious$: Subject<void> = new Subject<void>();
     private showSubject$: Subject<Product> = new Subject();
+    private destroy$: Subject<void> = new Subject();
 
-    show$: Observable<Product> = this.showSubject$.pipe(
+    public show$: Observable<Product> = this.showSubject$.pipe(
         takeUntil(this.destroy$)
     );
 
-    ngOnDestroy(): void {
-        this.destroy$.next();
-        this.destroy$.complete();
-    }
-
-    show(product: Product): Observable<void> {
+    /**
+     * Emits product to be shown in the quick view
+     */
+    public show(product: Product): Observable<void> {
         this.abortPrevious$.next();
 
         return of(this.showSubject$.next(product)).pipe(
-            map(() => {}),
             takeUntil(this.abortPrevious$)
         );
+    }
+
+    public ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
     }
 }
